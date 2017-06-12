dd77080b0f9bca36c67ad8d664dbed204b6b5a04
Rflpz <rflpzdev@gmail.com>
2017-06-11 23:34:07 -0500

    Files Author Renamed

--- Follow/RLLocation.h
+++ Follow/RLLocation.h
@@ -1,10 +1,12 @@
 //
 //  RLLocation.h
-//  SpotOn
+//  Follow
 //
-//  Created by Rafael Lopez on 10/21/16.
-//  Copyright © 2016 Rflpz. All rights reserved.
+//  Created by Rafael Lopez on 6/11/17.
+//  Copyright © 2017 Rflpz. All rights reserved.
 //
+
+
 
 #import <Foundation/Foundation.h>
 @import CoreLocation;
