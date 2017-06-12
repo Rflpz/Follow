5e6ba8dfe4f46b18c7167f805317b58c5d3fe2fc
Rflpz <rflpzdev@gmail.com>
2017-06-11 23:09:55 -0500

    Base project and Icons added

---
+++ Follow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Follow
+//
+//  Created by Rafael Lopez on 6/11/17.
+//  Copyright © 2017 Rflpz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
